9e208896513523980ef84676f66db602c5ef330a
Lazarte <P2175315@CORP.CHARTERCOM.com>
2019-12-04 16:58:14 -0600

    minor formatting

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import datetime, xlsxwriter
 from utils.dates import days, today	
 from utils.excel import writeDates, writeHeader, writeTimes
 
-
-
 workbook = xlsxwriter.Workbook('Timesheet ' + today + '.xlsx')
 worksheet = workbook.add_worksheet()
+
+worksheet.set_column(0, 0, 14)
+
 
 writeHeader(worksheet)
 writeDates(worksheet, days)
